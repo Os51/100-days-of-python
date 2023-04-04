6b4af901c86c7cae77790682a03ae7de54b31a05
Harry Osei <harry_osei201@msn.com>
2023-04-04 03:08:20 +0100

    Day 2, first lesson

---
+++ day-2/1_data_types.py
@@ -0,0 +1,36 @@
+# Data Types
+
+# Strings
+
+# Example of subscripting
+# The [number] value tells Python what letter "index" we would like to print
+# Python indexing starts at "0", which represents the first letter
+
+print("Hello"[0]) # H
+print("Hello"[1]) # e
+print("Hello"[2]) # l
+print("Hello"[3]) # l
+print("Hello"[4]) # o
+
+print("123") # Though this is a number, it is treated as a string due to the quotes
+print(type("123")) # returns as type 'str'
+
+# Integers
+# Whole numbers, no decimal places
+print(123 + 345)
+
+# Large Integers
+# We would normally use commas to split large integers
+# In Python, we replace the commas with underscores
+print(123_456_789)
+
+
+# Floats
+# Integers with Decimal places
+print(3.14159)
+
+
+# Booleans
+
+print(True)
+print(False)
