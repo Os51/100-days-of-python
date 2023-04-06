f12219621f3a4f013098f98a83d61470abc06212
Harry Osei <harry_osei201@msn.com>
2023-04-06 02:29:39 +0100

    reorg of folder structure

--- day-2/3_Data_Types_Challenge.py
+++ day-2/3_Data_Types_Challenge.py
@@ -1,5 +1,5 @@
 # Write a program that adds the digits in a 2 digit number.
-# E.G - if the input was 35, the output  should be 8 (3+5)
+# E.G - if the input was 35, the output should be 8 (3+5)
 two_digit_number = input("Enter a two digit number: ")
 first_digit = int(two_digit_number[0])
 second_digit = int(two_digit_number[1])
