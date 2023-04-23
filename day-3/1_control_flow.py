f0c21b4583bbf979e62c4b527f790cdd412de106
Harry Osei <harry_osei201@msn.com>
2023-04-23 01:33:52 +0100

    day 3
    control flow introduction

---
+++ day-3/1_control_flow.py
@@ -0,0 +1,20 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+# If Statements require the results to be indented. If they are not, you will get an indent error
+# The left side - if <variable> is the Equality Check. The right side after the operator is the assignment
+# If/Else is equiv to: "If the condition evaluates to True, do something, else do something else"
+if height > 120:
+    print("You can ride the rollercoaster")
+else:
+    print("Sorry, you have to be 120cm or more before you can ride.")
+
+"""
+Comparison Operators
+>   Greater than
+<   Less than
+>=  Greater than or equal to
+<=  Less than or equal to
+==  Equal to
+!=  Not equal to
+"""
